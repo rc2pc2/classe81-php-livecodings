22d457f9f7940b7557604882142104a21bd70ef7
rc2pc2 <petriccarcc@gmail.com>
2023-02-09 12:54:50 +0100

    laravel book simple implementation

---
+++ laravel-books/app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Book;
+
+class PageController extends Controller
+{
+
+    public function index(){
+        // logica qui
+
+        // $books = Book::all(); // select * from 'books'
+
+        // select * from 'books' where 'title' LIKE 'T%'
+        // $books = Book::where('title', 'LIKE', 'T%')->get();
+
+        // select * from 'books' where 'id' = 1
+        // $book = Book::find(1);
+
+        // $book = new Book();
+        // $book->title = 'La Gerusalemme Liberata';
+        // $book->author = 'Torquato Tasso';
+        // $book->cover = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/TassoGerusalem1724TP.jpg/220px-TassoGerusalem1724TP.jpg";
+        // $book->save();
+        $books = Book::all();
+
+
+        return view('home', compact('books'));
+    }
+}
---
+++ laravel-books/resources/views/home.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Homepage</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    <main class="bg-light">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-12">
+                    <h2>Main</h2>
+                </div>
+                @forelse ($books as $book)
+                <div class="col-4 p-4">
+                        <h5>
+                            {{ $book->title }}
+                        </h5>
+                        <h6>
+                            {{ $book->author }}
+                        </h6>
+                        <img src="{{ $book->cover }}" alt="" class="img-fluid">
+                    </div>
+                @empty
+                    <p>Non ci sono libri da mostrare</p>
+                @endforelse
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
